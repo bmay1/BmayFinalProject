7eb18c68f644254a3e22e69dec8fa678cf33aaa6
Bryan May <may.bryan11@gmail.com>
2018-12-02 17:46:27 -0600

    Left out : on Captial Label

--- BmayFinalProject/Form1.Designer.cs
+++ BmayFinalProject/Form1.Designer.cs
@@ -87,9 +87,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(50, 390);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Capital";
+            this.label2.Text = "Capital:";
             // 
             // txtYear
             // 
