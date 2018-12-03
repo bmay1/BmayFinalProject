a2f213568283451cbd1616a0c3d99c4dd0449325
Bryan May <may.bryan11@gmail.com>
2018-12-03 17:58:27 -0600

    Moved Insert, Update, Delete Button Down. Added label and textbox for ID.

--- BmayFinalProject/Form1.Designer.cs
+++ BmayFinalProject/Form1.Designer.cs
@@ -44,6 +44,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -157,7 +159,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(794, 56);
+            this.button1.Location = new System.Drawing.Point(798, 125);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 74);
             this.button1.TabIndex = 13;
@@ -166,7 +168,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(794, 158);
+            this.button2.Location = new System.Drawing.Point(798, 227);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(127, 74);
             this.button2.TabIndex = 14;
@@ -175,18 +177,36 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(794, 250);
+            this.button3.Location = new System.Drawing.Point(798, 319);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(127, 74);
             this.button3.TabIndex = 15;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(795, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(792, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 22);
+            this.textBox1.TabIndex = 17;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(973, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -229,6 +249,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
