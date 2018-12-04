7a1baa9242b3c6c165c61e87e3de53467798d751
Bryan May <may.bryan11@gmail.com>
2018-12-03 20:21:53 -0600

    Orginal Coding

--- BmayFinalProject/Form1.cs
+++ BmayFinalProject/Form1.cs
@@ -22,12 +22,12 @@ namespace BmayFinalProject
         {
             List<State> list = collection.AsQueryable().ToList<State>();
             dataGridView1.DataSource = list;
-            txtState.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
-            txtCapital.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
-            txtYear.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
-            txtMammal.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
-            txtBird.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
-            txtGovernor.Text = dataGridView1.Rows[0].Cells[5].Value.ToString();
+            txtState.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            txtCapital.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            txtYear.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
+            txtMammal.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
+            txtBird.Text = dataGridView1.Rows[0].Cells[5].Value.ToString();
+            txtGovernor.Text = dataGridView1.Rows[0].Cells[6].Value.ToString();
 
         }
         public Form1()
@@ -38,12 +38,12 @@ namespace BmayFinalProject
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            txtState.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
-            txtCapital.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
-            txtYear.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
-            txtMammal.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
-            txtBird.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
-            txtGovernor.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txtState.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtCapital.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtYear.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtMammal.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtBird.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txtGovernor.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
