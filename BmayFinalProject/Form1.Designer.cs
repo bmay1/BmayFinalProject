1dc512cce916bb9f647bc4b3ff34a4717f2000e4
Bryan May <may.bryan11@gmail.com>
2018-12-03 19:59:35 -0600

    Added in code for Form1

--- BmayFinalProject/Form1.Designer.cs
+++ BmayFinalProject/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,6 +60,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(704, 328);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // label1
             // 
@@ -165,6 +166,7 @@
             this.button1.TabIndex = 13;
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -174,6 +176,7 @@
             this.button2.TabIndex = 14;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -183,6 +186,7 @@
             this.button3.TabIndex = 15;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label7
             // 
@@ -193,20 +197,20 @@
             this.label7.TabIndex = 16;
             this.label7.Text = "ID:";
             // 
-            // textBox1
+            // txtId
             // 
-            this.textBox1.Location = new System.Drawing.Point(792, 60);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(152, 22);
-            this.textBox1.TabIndex = 17;
+            this.txtId.Location = new System.Drawing.Point(792, 60);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(152, 22);
+            this.txtId.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(973, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtId);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -251,7 +255,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
 
--- BmayFinalProject/Form1.cs
+++ BmayFinalProject/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,56 @@ namespace BmayFinalProject
 {
     public partial class Form1 : Form
     {
+        static MongoClient state = new MongoClient();
+        static IMongoDatabase db = state.GetDatabase("States");
+        static IMongoCollection<State> collection = db.GetCollection<State>("States");
+        
+        public void ReadAllDocuments()
+        {
+            List<State> list = collection.AsQueryable().ToList<State>();
+            dataGridView1.DataSource = list;
+            txtState.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
+            txtCapital.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            txtYear.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            txtMammal.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
+            txtBird.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
+            txtGovernor.Text = dataGridView1.Rows[0].Cells[5].Value.ToString();
+
+        }
         public Form1()
         {
             InitializeComponent();
+            ReadAllDocuments();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtState.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtCapital.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtYear.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtMammal.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtBird.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtGovernor.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            State s = new State(txtState.Text, txtCapital.Text, Int32.Parse(txtYear.Text), txtMammal.Text, txtBird.Text, txtGovernor.Text);
+            collection.InsertOne(s);
+            ReadAllDocuments();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var updateDB = Builders<State>.Update.Set("state", txtState.Text).Set("capital", txtCapital.Text).Set("year", txtYear.Text).Set("mammal", txtMammal.Text).Set("Bird", txtBird.Text).Set("governor", txtGovernor.Text);
+            collection.UpdateOne(s => s.Id == ObjectId.Parse(txtId.Text), updateDB);
+            ReadAllDocuments();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            collection.DeleteOne(s => s.Id == ObjectId.Parse(txtId.Text));
+            ReadAllDocuments();
         }
     }
 }
