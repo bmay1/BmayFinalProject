d317bb73cf9e52ce9c02c989e5fc617ce5f496c5
Bryan May <may.bryan11@gmail.com>
2018-12-04 17:23:55 -0600

    Create class called States and created Constructor

---
+++ BmayFinalProject/States.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmayFinalProject
+{
+    class States
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("state")]
+        public String State { get; set; }
+        [BsonElement("capital")]
+        public String Capital {get; set;}
+        [BsonElement("year")]
+        public int Year {get; set;}
+        [BsonElement("mammal")]
+        public String Mammal {get; set;}
+        [BsonElement("bird")]
+        public String Bird{get; set;}
+        [BsonElement("governor")]
+        public String Governor {get; set;}
+
+        public States(string state, string capital, int year, string mammal, string bird, string governor)
+        {
+            State = state;
+            Capital = capital;
+            Year = year;
+            Mammal = mammal;
+            Bird = bird;
+            Governor = governor;
+        }
+    }
+}
