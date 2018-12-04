922ccaaaaa5f591a2af48a46c362efe18ec0bce9
Bryan May <may.bryan11@gmail.com>
2018-12-03 19:14:25 -0600

    BsonID, BsonElement and constructor

--- BmayFinalProject/State.cs
+++ BmayFinalProject/State.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,29 @@ namespace BmayFinalProject
 {
     class State
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("state")]
+        public String States { get; set; }
+        [BsonElement("capital")]
+        public String Capital { get; set; }
+        [BsonElement("year")]
+        public Int32 Year { get; set; }
+        [BsonElement("mammal")]
+        public String Mammal { get; set; }
+        [BsonElement("bird")]
+        public String Bird { get; set; }
+        [BsonElement("governor")]
+        public String Governor { get; set; }
+
+        public State(string states, string capital, int year, string mammal, string bird, string governor)
+        {
+            States = states;
+            Capital = capital;
+            Year = year;
+            Mammal = mammal;
+            Bird = bird;
+            Governor = governor;
+        }
     }
 }
