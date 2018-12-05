4ce0afb3c24368ad6e3c691c8ae289c447edd7c8
Bryan May <may.bryan11@gmail.com>
2018-12-04 18:09:04 -0600

    Attempted to fix error code by moving Id at the bottom.

--- BmayFinalProject/Form1.cs
+++ BmayFinalProject/Form1.cs
@@ -21,13 +21,13 @@ namespace BmayFinalProject
         public void ReadAllDcouments()
         {
             List<States> list = collection.AsQueryable().ToList<States>();
-            txtId.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
-            txtState.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
-            txtCapital.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
-            txtYear.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
-            txtMammal.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
-            txtBird.Text = dataGridView1.Rows[0].Cells[5].Value.ToString();
-            txtGovernor.Text = dataGridView1.Rows[0].Cells[6].Value.ToString();
+            txtState.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
+            txtCapital.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            txtYear.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+            txtMammal.Text = dataGridView1.Rows[0].Cells[3].Value.ToString();
+            txtBird.Text = dataGridView1.Rows[0].Cells[4].Value.ToString();
+            txtGovernor.Text = dataGridView1.Rows[0].Cells[5].Value.ToString();
+            txtId.Text = dataGridView1.Rows[0].Cells[6].Value.ToString();
         }
 
         public Form1()
